0158d20ae1d9312c2637fbad23023250ecc51544
Ulsah <udalexk@gmail.com>
2019-10-02 13:44:57 +0530

    installed sweetaler

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,16 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import swal from 'sweetalert2';
+window.swal=swal;
+const toast=swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 6000
+});
+window.toast = toast;
+
 const app = new Vue({
     el: '#app',
 });
